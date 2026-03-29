4314f9dc7c4042cc293a50c8d6053d1c5ab69e82
daimondF2 <jmyee03@gmail.com>
2026-03-29 23:12:34 +1100

    Learning triangle (vertices forming 3d vector for 2d plane or whatever) vertex shaders and fragment shaders and combining them. doesnt work yet

--- Game Enigne/Game Enigne.cpp
+++ Game Enigne/Game Enigne.cpp
@@ -14,19 +14,37 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height) //for 
 
 }
 
-
 void processInput(GLFWwindow* window) //handles inputs
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) //gets key identify if escape pressed if true close window
         glfwSetWindowShouldClose(window, true);
 }
+// DONT TOUCH ABOVE THIS IS JUST FOR THE WINDOW
+//GLSL has a vector datatype that contains 1 to 4 floats based on its postfix digit.
+//Since each vertex has a 3D coordinate we create a vec3 input variable with the name aPos.
+// We also specifically set the location of the input variable via layout(location = 0)
+
+
+const char* vertexShaderSource = "#version 330 core\n" //basic vertex shader
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";      
+
+const char* fragmentShaderSource = "#version 330 core\n" //fragment shader puts color on triangle
+"out vec4 FragColor;\n"
+"void main() {\n"
+"FragColor = vec4(1.0, 0.5, 0.2, 1.0); // orange\n"
+"}\0";
+
 int main()
 {
+    //glfw setup
     if (!glfwInit()) {
         std::cout << "Failed initialize glfw" << std::endl;
         return -1;
     }
-
     // Tell GLFW we want OpenGL 3.3
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -42,8 +60,8 @@ int main()
         return -1;
     }
     glfwMakeContextCurrent(window);
-
-
+    
+    //glad start
     //GLAD manages function pointers for OpenGL so we want to initialize GLAD before we call any OpenGL function:
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
@@ -53,8 +71,69 @@ int main()
     //tells OpenGL the size of the rendering window so OpenGL knows how we want to display the data and coordinates with respect to the window.
     glViewport(0, 0, 800, 600); //The first two parameters of glViewport set the location of the lower left corner of the window. 
     //The third and fourth parameter set the width and height of the rendering window in pixels, which we set equal to GLFW's window size.
-
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback); //for resize
+    
+    //ABOVE IS STANDARD
+    //compile shaders
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER); //provide the type of shader we want to create as an argument to glCreateShader. Since we're creating a vertex shader we pass in GL_VERTEX_SHADER.
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    //check for error
+    int  success;
+    char infoLog[512];//check if compilation was successful with glGetShaderiv. 
+    //If compilation failed, we should retrieve the error message with glGetShaderInfoLog and print the error messag
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+    //NEW CODE AND FUNC
+    //compile fragment shader
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    //check for error
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+    //shader program links the shaders
+    unsigned int shaderProgram = glCreateProgram(); //creates func and returns ID ref to new created prog obj
+    glAttachShader(shaderProgram, vertexShader); //attaching and linking shaders together
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    //check errors
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "Shader link error: " << infoLog << std::endl;
+    }
+    //deleting shaders because we linked and dont need them
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+    
+
+    //defining a triangle vertices
+    float vertices[] = {
+    -0.5f, -0.5f, 0.0f,
+     0.5f, -0.5f, 0.0f,
+     0.0f,  0.5f, 0.0f
+    };
+    //vertex buffer (first occurance of an openGl object) -unique id correspoding to the buffer
+    unsigned int VBO;
+    glGenBuffers(1, &VBO); // generates ID
+    //binds the buffers
+    glBindBuffer(GL_ARRAY_BUFFER, VBO); // array buffer allows bind several buffers at once as long as they are different used for vertex buffer obj
+
+    //any buffer calls we make (on the GL_ARRAY_BUFFER target) will be used to configure the currently bound buffer, 
+    //which is VBO. Then we can make a call to the glBufferData function that copies the previously defined vertex data into the buffer's memory:
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); //glBufferData is a function specifically targeted to copy user-defined data into the currently bound buffer.
+    //telling OPENGL how to read vertexesd
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);//(which vertex(location), size of attribute, data type, data normalised y/n, 
+    glEnableVertexAttribArray(0);
 
     while (!glfwWindowShouldClose(window)) //tells the window to keep drawing imagesa and not single image
     {   
@@ -64,6 +143,9 @@ int main()
         // rendering commands here
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f); //sets screen color
         glClear(GL_COLOR_BUFFER_BIT);
+        //DRAW TRIANGLE
+        glUseProgram(shaderProgram);// using shaders
+
 
         // check and call events and swap the buffers
         glfwSwapBuffers(window);
